08a8ae3ad6da8f5afd6b60ab6efe8136a26947e2
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-27 13:44:42 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time)
+{
+  let arr = time.split(":");
+  if (parseInt(arr[0]) < 12)
+  {
+    return "Good Morning"
+  }
+  else
+  {
+    if (parseInt(arr[0]) > 17)
+    {
+      return "Good Evening"
+    }
+    else
+    {
+      return "Good Afternoon"
+    }
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(text)
+{
+  let ele = document.getElementById("greeting");
+  ele.innerText = text
+}
